8897e795a1534133632c6321426d997e3a2c3ef5
hohoseohyun <hohoseohyun@gmail.com>
2022-01-11 13:44:07 +0900

    first commit by hobi

---
+++ src/App/index.js
@@ -0,0 +1,55 @@
+import React, { Component, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
+
+import '../../node_modules/font-awesome/scss/font-awesome.scss';
+
+import Loader from './layout/Loader'
+import Aux from "../hoc/_Aux";
+import ScrollToTop from './layout/ScrollToTop';
+import routes from "../route";
+import Axios from "axios";
+
+const AdminLayout = Loadable({
+    loader: () => import('./layout/AdminLayout'),
+    loading: Loader
+});
+
+class App extends Component {
+    render() {
+        Axios.get("/api/sample").then((response) => {
+            if (response.data) {
+                console.log('hobi', response.data);
+            } else {
+                alert("fail");
+            }
+        })
+        const menu = routes.map((route, index) => {
+          return (route.component) ? (
+              <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  name={route.name}
+                  render={props => (
+                      <route.component {...props} />
+                  )} />
+          ) : (null);
+        });
+
+        return (
+            <Aux>
+                <ScrollToTop>
+                    <Suspense fallback={<Loader/>}>
+                        <Switch>
+                            {menu}
+                            <Route path="/" component={AdminLayout} />
+                        </Switch>
+                    </Suspense>
+                </ScrollToTop>
+            </Aux>
+        );
+    }
+}
+
+export default App;
